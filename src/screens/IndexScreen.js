7dfa98e1b47a94689e85e4a5a13a9312498b1ae2
petercolella <peter.joseph.colella@gmail.com>
2020-06-01 21:29:05 -0400

    126. adds value prop to context provider and FlatList to IndexScreen

--- src/screens/IndexScreen.js
+++ src/screens/IndexScreen.js
@@ -1,10 +1,19 @@
-import React from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import React, { useContext } from 'react';
+import { Text, View, FlatList, StyleSheet } from 'react-native';
+import BlogContext from '../context/BlogContext';
 
 const IndexScreen = () => {
+  const blogPosts = useContext(BlogContext);
+
   return (
     <View>
-      <Text>Index Screen</Text>
+      <FlatList
+        data={blogPosts}
+        keyExtractor={blogPost => blogPost.title}
+        renderItem={({ item }) => {
+          return <Text>{item.title}</Text>;
+        }}
+      />
     </View>
   );
 };
