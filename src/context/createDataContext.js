7d5bb1e92c8769a432e1eeb30f7f35edfb70f298
petercolella <peter.joseph.colella@gmail.com>
2020-06-08 23:36:20 -0400

    updates useReducer to useAsyncReducer in createDataContext

--- src/context/createDataContext.js
+++ src/context/createDataContext.js
@@ -1,10 +1,11 @@
-import React, { useReducer } from 'react';
+import React from 'react';
+import useAsyncReducer from '../hooks/useAsyncReducer';
 
 export default (reducer, actions, initialState) => {
   const Context = React.createContext();
 
   const Provider = ({ children }) => {
-    const [state, dispatch] = useReducer(reducer, initialState);
+    const [state, dispatch] = useAsyncReducer(reducer, initialState);
 
     const boundActions = {};
 
