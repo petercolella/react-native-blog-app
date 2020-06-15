9f98f6114aa987959bfac6a0c31e0b512d2771d1
petercolella <peter.joseph.colella@gmail.com>
2020-06-14 23:38:28 -0400

    adds Form component

---
+++ src/components/Form.js
@@ -0,0 +1,64 @@
+import React from 'react';
+import { Button, Text, TextInput, View, StyleSheet } from 'react-native';
+
+const Form = ({
+  title,
+  setTitle,
+  content,
+  setContent,
+  buttonTitle,
+  handleSubmit
+}) => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.label}>Title:</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          autoCorrect={false}
+          placeholder="Enter title"
+          style={styles.inputStyle}
+          value={title}
+          onChangeText={setTitle}
+        />
+      </View>
+      <Text style={styles.label}>Content:</Text>
+      <View style={styles.inputWrapper}>
+        <TextInput
+          autoCorrect={false}
+          placeholder="Enter content"
+          style={styles.inputStyle}
+          value={content}
+          onChangeText={setContent}
+        />
+      </View>
+      <View style={styles.buttonWrapper}>
+        <Button title={buttonTitle} onPress={handleSubmit} />
+      </View>
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  buttonWrapper: {
+    alignItems: 'center'
+  },
+  container: {
+    flex: 1,
+    margin: 5
+  },
+  inputStyle: {
+    borderColor: 'black',
+    borderWidth: 1,
+    fontSize: 18,
+    padding: 10
+  },
+  inputWrapper: {
+    height: 75
+  },
+  label: {
+    fontSize: 20,
+    marginBottom: 5
+  }
+});
+
+export default Form;
